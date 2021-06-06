2614a94eee04c0afd1be68fe87bf7ecacd6e69cb
Matvejs Timosenko <matthew.timoshenko@gmail.com>
2021-06-06 09:39:52 +0300

    Adding form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: any = {
+    name: '',
+    surname: '',
+    email: '',
+  }
+
+  errorMessage: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFormValidate(): void {
+    console.log(this.registerForm);
+
+    // validate name
+    // validate surname
+    // validate email
+
+    // if something not ok - show error message
   }
 
 }
